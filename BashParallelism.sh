5aa2bd3c72148cab273ac0faa42ab569de66a2ce
Raymond C. Borges Hink <borgesraymond@gmail.com>
2016-05-26 17:47:31 -0400

    Fixed formatting

--- BashParallelism.sh
+++ BashParallelism.sh
@@ -4,16 +4,16 @@
 
 
 while read line           
-			do
-              parallelism=$1 #Input number of threads from user
-              
-              eval $line & #<-This ampersand Parallelizes the process eval $someComplexLinuxCommand
-              
-              #Parallelize malicious conversation filter
-              NPROC=$(($NPROC+1))
-              if [ "$NPROC" -ge "$parallelism" ]; then
-                echo "Waiting for $parallelism tshark processes to finish..."
-                wait
-                NPROC=0
-              fi #Done parallelism 
-      done < inputFile.txt
+do
+	parallelism=$1 #Input number of threads from user
+	
+	eval $line & #<-This ampersand Parallelizes the process eval $someComplexLinuxCommand
+	
+	#Parallelize malicious conversation filter
+	NPROC=$(($NPROC+1))
+	if [ "$NPROC" -ge "$parallelism" ]; then
+		echo "Waiting for $parallelism tshark processes to finish..."
+		wait
+		NPROC=0
+	fi #Done parallelism 
+done < inputFile.txt
