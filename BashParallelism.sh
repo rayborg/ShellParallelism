16db4b8bcc20855e83fd0033422cb448a3adc9c9
Raymond C. Borges Hink <borgesraymond@gmail.com>
2016-05-26 17:46:42 -0400

    Initial commit of exaple
    Goes through a file of commands and executes them in parallel using the number of threads specified in the input parameter

---
+++ BashParallelism.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Example: ./BashParallelism.sh 20
+#This will run some command given in the code the number of times specified in the input parameter
+
+
+while read line           
+			do
+              parallelism=$1 #Input number of threads from user
+              
+              eval $line & #<-This ampersand Parallelizes the process eval $someComplexLinuxCommand
+              
+              #Parallelize malicious conversation filter
+              NPROC=$(($NPROC+1))
+              if [ "$NPROC" -ge "$parallelism" ]; then
+                echo "Waiting for $parallelism tshark processes to finish..."
+                wait
+                NPROC=0
+              fi #Done parallelism 
+      done < inputFile.txt
